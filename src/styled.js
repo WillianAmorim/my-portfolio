0628f728017eda9ae8b1d53c13dfc1a7726e071c
Willian <willian.amorim1@outlook.com>
2023-03-18 00:16:01 -0300

    adiconado estilização no component header

---
+++ src/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Header = styled.header `
+  background-color: yellow;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15px;
+  align-items: center;
+
+  ul {
+    display: flex;
+    list-style: none;
+    gap: 30px;
+  }
+`

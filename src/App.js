9845660826493c01ec9c221103e40aa888da7e25
Willian <willian.amorim1@outlook.com>
2023-03-28 16:51:57 -0300

    refazendo componentes do projeto

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from "react";
-import { Main, Header, Div } from './styled'
-import Gif from './assets/giphy.gif'
-import Image from './assets/Willian.jpeg'
+import Header from './components/Header'
+// import Image from './assets/Willian.jpeg'
 
 function App() {
   return (
-    <Main>
-      <Header>
-        <p>Portfolio</p>
-        <ul>
-          <li>Home</li>
-          <li>Sobre Mim</li>
-          <li>Projetos</li>
-          <li>Contate-me</li>
-        </ul>
-      </Header>
-      <Div>
-        <img id="gif-computer" src={Gif} alt="" />
-        <section>
-          <h1>Willian SIlva Amorim</h1>
-          <h2>Desenvolvedor Front-End</h2>
-        </section>
-      </Div>
-    </Main>
+    <>
+      <Header />
+    </>
   );
 }
 

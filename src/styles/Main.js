6a7ada6b7d0c4d485c11384aa7ea86802e54d7d7
Willian <willian.amorim1@outlook.com>
2023-03-28 17:33:33 -0300

    adicionado componente styled-component do Main

---
+++ src/styles/Main.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: bisque;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`

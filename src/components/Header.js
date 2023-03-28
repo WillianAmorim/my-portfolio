b3d70d58b8743c12491829a3dcd3be8ab8fae614
Willian <willian.amorim1@outlook.com>
2023-03-28 16:54:12 -0300

    feito estrutura do Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      
+      <div>Portifólio</div>
+      <ul>
+        <li>Home</li>
+        <li>Sobre Mim</li>
+        <li>Projetos</li>
+        <li>Contate-me</li>
+      </ul>
     </div>
   )
 }

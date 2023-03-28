d7442f7523d5321787654db39c3bf9d73b485b64
Willian <willian.amorim1@outlook.com>
2023-03-28 19:00:27 -0300

    adicionado Componet do styled components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import { Container } from "./styles/App";
 
 function App() {
   return (
-    <>
+    <Container>
       <Header />
       <Main />
       <Footer />
-    </>
+    </Container>
   );
 }
 

c51ad6734f3cf1c86934e053ebab7f9bad2e9fb7
Willian <willian.amorim1@outlook.com>
2023-03-29 02:04:33 -0300

    adicionado estilização para elementos do main

--- src/styles/Main.js
+++ src/styles/Main.js
@@ -5,17 +5,30 @@ export const Container = styled.div `
   justify-content: space-evenly;
   align-items: center;
 
-  section {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
 
   #info-home {
     display: flex;
     flex-direction: column;
     gap: 20px;
+
+    h1 {
+      font-size: 35px;
+      text-align: center;
+      font-weight: normal;
+      line-height: 1.2;
+    }
+
+    p {
+      font-size: 35px;
+      text-align: center;
+    }
+
+    section {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
   }
 
   #section-links {

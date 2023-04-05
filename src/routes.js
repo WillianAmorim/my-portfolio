db89afb55a034144e35bf46959c55932f543ce5c
Willian <willian.amorim1@outlook.com>
2023-04-05 11:39:29 -0300

    add arquivo de rotas no react

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const Routes = () => {
+  <Router>
+    <Switch>
+      <Route exact path="/" />
+    </Switch>
+  </Router>
+}
+
+export default Routes
